36d8fa4248eced416d9b7d51ea43cdf559a11bbe
Guilherme Santos <31636389+GuilhermeSnts@users.noreply.github.com>
2020-08-04 23:55:54 -0300

    Update lastRequest.vue
    fixing unbreakable word

--- app-delivery/src/components/lastRequest.vue
+++ app-delivery/src/components/lastRequest.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <div>
     <span class="text-weight-bold text-subtitle1">Histórico</span>
     <div class="scrolling-wrapper">
       <q-card
@@ -8,24 +8,20 @@
         :key="index"
       >
         <q-card-section>
-      <div class="row">
-        <div class="col-4">
-          paaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </div>
-        <div class="col q-ml-sm text-subtitle2 text-weight-bold">
-         
-        </div>
-      </div>
-      <div class="row q-mt-sm">
-        <q-btn
-          dense
-          class="full-width"
-          color="primary"
-          style="font-size:2vh"
-          label="Adicionar ao Carrinho"
-        />
-      </div>
-    </q-card-section>
+          <div class="row text">
+            paaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </div>
+          <div class="col q-ml-sm text-subtitle2 text-weight-bold"></div>
+          <div class="row q-mt-sm">
+            <q-btn
+              dense
+              class="full-width"
+              color="primary"
+              style="font-size:2vh"
+              label="Adicionar ao Carrinho"
+            />
+          </div>
+        </q-card-section>
       </q-card>
     </div>
   </div>
@@ -48,13 +44,14 @@ export default {
 .scrolling-wrapper {
   overflow-x: scroll;
   overflow-y: hidden;
-  
 }
 .hzreq {
-  white-space: nowrap;
   display: inline-block;
   width: 55vw;
   height: 30vh;
 }
-
+.text{
+  white-space: normal;
+  word-break: break-all;
+}
 </style>
